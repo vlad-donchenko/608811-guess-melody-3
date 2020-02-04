a79a804325e5e70ea1ff596726e1b6eb108d10c8
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-02-04 22:06:40 +0200

    module1-task2 - Отрендерит компонент Welcome

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
 
+const ERRORS_COUNT = 3;
+
+const root = document.querySelector(`#root`);
+ReactDOM.render(<App errorsCount={ERRORS_COUNT}/>, root);
