0a0ba8ecbd1c3ee1ff2425ab9b3b0eb7cc7971b6
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-02-11 19:06:48 +0200

    module2-task2 - Добавляет новый props и обновляет snapshots

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import PropTypes from "prop-types";
 
+const onWelcomeButtonClick = () => {};
+
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount}/>
+    <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonClick={onWelcomeButtonClick}/>
   );
 };
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired,
 };
 
 export default App;
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -6,6 +6,7 @@ it(`Render App`, () => {
   const tree = renderer
     .create(<App
       errorsCount={3}
+      onWelcomeButtonClick={() => {}}
     />)
     .toJSON();
 
