ffe4836c14ae84d4d0c66ef75d134d1803d7632b
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-02-15 18:26:58 +0200

    module3-task1 - Передает моки в App и валидирует

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -14,6 +14,7 @@ const App = (props) => {
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import {questions} from "./mocks/questions";
 
 const ERRORS_COUNT = 3;
 
 const root = document.querySelector(`#root`);
-ReactDOM.render(<App errorsCount={ERRORS_COUNT}/>, root);
+ReactDOM.render(<App errorsCount={ERRORS_COUNT} questions={questions}/>, root);
